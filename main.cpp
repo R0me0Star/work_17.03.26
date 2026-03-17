78d2e198dcb1647738aad456c18c13cd8162d797
R0me0Star <eip_spb@mail.ru>
2026-03-17 13:16:47 +0300

    main update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "top-it-vector.hpp"
 
+bool testDefaultVector() {
+  Vector< int > v:
+  return v.isEmpty();
+}
+
 int main()
 {
-  Vector< int > v;
-  bool c = v.isEmpty();
-  return !c;
+  using test_t = bool(*)();
+  test_t tests[] = {
+    testDefaultVector
+  };
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  for (size_t i = 0; i < count; ++i) {
+    bool res = tests[i]();
+    std::cout << "Result: " << res << "\n";
+  }
 }
+
 
 
