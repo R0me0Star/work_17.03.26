a70326da785d0d4af00d594a24e88ef65ad89a7c
R0me0Star <eip_spb@mail.ru>
2026-03-17 12:55:22 +0300

    makefile & gitignore

---
+++ top-it-vector.hpp
@@ -0,0 +1,15 @@
+#ifndef TOP_IT_VECTOR_HPP
+#define TOP_IT_VECTOR_HPP
+
+namespace topit {
+  template< class T >
+  struct Vector {
+
+   private:
+     T *data_;
+     size_t size_, capacity_;
+  };
+}
+
+#endif
+
