a0722cd8829c834d54413a8358f0324f93d40585
R0me0Star <eip_spb@mail.ru>
2026-03-23 20:30:58 +0300

    updating main & top-it-vector

--- main.cpp
+++ main.cpp
@@ -1,22 +1,58 @@
 #include "top-it-vector.hpp"
+#include <iostream>
 
-bool testDefaultVector() {
-  Vector< int > v:
+bool testDefaultVector()
+{
+  topit::Vector< int > v;
   return v.isEmpty();
+}
+
+bool testVectorWithValue()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  return !v.isEmpty();
+}
+
+bool testElementAccess()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  return v[0] == 1 && v[1] == 2;
+}
+
+bool testCopyConstructor()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  topit::Vector< int > yav = v;
+  bool isAllEqual = v.getSize() == yav.getSize();
+  for (size_t i = 0; isAllEqual && i < v.getSize(); ++i)
+  {
+    isAllEqual = isAllEqual && (v[i] == yav[i]);
+  }
+  return isAllEqual;
 }
 
 int main()
 {
-  using test_t = bool(*)();
-  test_t tests[] = {
-    testDefaultVector
-  };
-  const size_t count = sizeof(tests) / sizeof(test_t);
-  for (size_t i = 0; i < count; ++i) {
-    bool res = tests[i]();
-    std::cout << "Result: " << res << "\n";
+  using test_t = bool (*)();
+  using pair_t = std::pair< const char*, test_t >;
+  pair_t tests[] = { {"Default vector should be empty", testDefaultVector},
+                    {"Vector with any value is not empty", testVectorWithValue},
+                    {"Inbound access elements", testElementAccess},
+                    {"Sizes must be equal as elements", testCopyConstructor} };
+  const size_t count = sizeof(tests) / sizeof(pair_t);
+  std::cout << std::boolalpha;
+  bool pass = true;
+  for (size_t i = 0; i < count; ++i)
+  {
+    bool res = tests[i].second();
+    std::cout << res << ": ";
+    std::cout << tests[i].first << '\n';
+    pass = pass && res;
   }
+  std::cout << pass;
+  std::cout << ": RESULT\n";
 }
-
-
-
