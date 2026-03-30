81a78fe874ec598e2e362499d5c5ff4463bee318
R0me0Star <eip_spb@mail.ru>
2026-03-30 10:19:41 +0300

    yamain

---
+++ yamain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <initilazier_list>
+#include "top-it-vector.hpp"
+
+int main()
+{
+  std::initializer_list< int > il = {1, 2, 3, 4, 5, 6};
+
+  topit::Vector< int > v = {1, 2, 3, 4, 5};
+}
+
